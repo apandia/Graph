01b622926acbe4a9b71a3505be39befbd5c5220f
apandia <96688353+apandia@users.noreply.github.com>
2022-06-04 13:55:19 +0530

    Add files via upload

---
+++ NumberOfIsland.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Find the number of islands(1s)
+/*
+Input1:
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Input2:
+ grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+*/
+//TC: O(N)    [O(5N) worst case a single node is traversed 5 times, 1 processing and 4 from side nodes.]
+//SC: O(1)    [if recursive stack space is not considered.]
+//SC: O(MxN)  [if recursive space is considered as there will be MxN recursive call.]
+void markIslands(vector<vector<char>>&grid, int i, int j, int numRows, int numColumns)
+{
+    if(i <0 || i >= numRows || j < 0 || j >= numColumns || grid[i][j] != '1')
+        return;
+
+    grid[i][j] = '2';
+
+    markIslands(grid, i-1, j, numRows, numColumns);
+    markIslands(grid, i+1, j, numRows, numColumns);
+    markIslands(grid, i, j-1, numRows, numColumns);
+    markIslands(grid, i, j+1, numRows, numColumns);
+}
+
+int numIslands(vector<vector<char>>& grid)
+{
+    int numRows =grid.size();
+    if(numRows == 0)
+        return 0;
+
+    int numColumns = grid[0].size(); //all rows have same number of column
+
+    int numIslands = 0;
+
+    for(int i = 0; i < numRows; i++)
+    {
+        for(int j = 0; j < numColumns; j++)
+        {
+            if(grid[i][j] == '1')
+            {
+                markIslands(grid, i, j, numRows, numColumns);
+                numIslands++;
+            }
+        }
+    }
+
+    return numIslands;
+}
+
+int main()
+{
+    vector<vector<char>> grid1 = {{'1','1','1','1','0'},
+                                  {'1','1','0','1','0'},
+                                  {'1','1','0','0','0'},
+                                  {'0','0','0','0','0'}
+                                 };
+
+    vector<vector<char>> grid2 = {{'1','1','0','0','0'},
+                                  {'1','1','0','0','0'},
+                                  {'0','0','1','0','0'},
+                                  {'0','0','0','1','1'}
+                                 };
+
+    cout<<"Num islands for grid1 = "<<numIslands(grid1)<<endl;
+    cout<<"Num islands for grid2 = "<<numIslands(grid2)<<endl;
+
+    return 0;
+}
